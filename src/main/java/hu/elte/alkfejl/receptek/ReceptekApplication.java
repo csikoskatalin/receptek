2913599ee49ca0ebaa6477deb7ab91422b9beca0
csikoskatalin <pallo.katalin@gmail.com>
2017-10-22 15:48:10 +0200

    init

---
+++ src/main/java/hu/elte/alkfejl/receptek/ReceptekApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.alkfejl.receptek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptekApplication.class, args);
+	}
+}
